48de2b8ec86637a716d68d0e8ce3977b0dae5e96
Mastan Patel <m.mastanpatel@gmail.com>
2022-01-22 11:43:46 +0530

    added reamore showless button

--- src/Tour.js
+++ src/Tour.js
@@ -5,7 +5,7 @@ import React, { useState } from 'react';
 const Tour = ({tour}) => {
 
   //const [Onetour, setOnetour] = useState(tour);
-
+  const [readmore, setreadmore] = useState(false);
   const {id, name,info,image,price} = tour
 
   return (
@@ -16,7 +16,10 @@ const Tour = ({tour}) => {
         <h4>{name}</h4>
         <h4 className='tour-price'>${price}</h4>
         </div>
-        <p>{info}</p>
+        <p>
+          {readmore ? info : `${info.substring(0,200)}...`}
+          <button onClick={() => setreadmore(!readmore)}>{readmore ? 'show less': 'read more'}</button>
+          </p>
         <button className='delete-btn'>Not Interested</button>
       </footer>
     </article>
